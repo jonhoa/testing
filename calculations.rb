06cf99722a5d57890ad747210cbfae0ede2a3791
Jonathan <jonitoadriano@gmail.com>
2023-01-30 11:35:02 -0600

    Calculator methods

---
+++ calculations.rb
@@ -0,0 +1,25 @@
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+  def square(number)
+    return square * square
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
